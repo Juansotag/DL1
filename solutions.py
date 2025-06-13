e2c8bf4a0fc7fcf1eeba07b3916fc9cba00d2c56
Juan Sotelo Aguilar <78369247+Juansotag@users.noreply.github.com>
2025-06-12 19:33:48 -0500

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -7,50 +7,49 @@ def optimize_torch_fun1(f):
     Fast optimization of a PyTorch function with 2D input.
     """
     x = torch.rand(2, requires_grad=True)
-    opt = torch.optim.Adam([x], lr=0.035)
+    opt = torch.optim.SGD([x], lr=0.035)
     for _ in range(67):
         opt.zero_grad()
         loss = f(x)
         loss.backward()
         opt.step()
-    return x
+    return torch.tensor(x.detach().numpy())
 
 def optimize_torch_fun2(f):
     """
     Fast optimization of a PyTorch function with 10D input.
     """
     x = torch.rand(10, requires_grad=True)
-    opt = torch.optim.Adam([x], lr=0.031)
+    opt = torch.optim.SGD([x], lr=0.031)
     for _ in range(75):
         opt.zero_grad()
         loss = f(x)
         loss.backward()
         opt.step()
-    return x
+    return torch.tensor(x.detach().numpy())
 
 def optimize_tf_fun1(f):
     """
     Fast optimization of a TensorFlow function with 2D input.
     """
     x = tf.Variable(tf.random.uniform((2,), -1, 1))
-    opt = tf.keras.optimizers.Adam(learning_rate=0.032)
+    opt = tf.keras.optimizers.SGD(learning_rate=0.032)
     for _ in range(70):
         with tf.GradientTape() as tape:
             loss = f(x)
         grads = tape.gradient(loss, x)
         opt.apply_gradients([(grads, x)])
-    return x
+    return tf.Variable(initial_value=x.numpy(), dtype=tf.float32)
 
 def optimize_tf_fun2(f):
     """
     Fast optimization of a TensorFlow function with 10D input.
     """
     x = tf.Variable(tf.random.uniform((10,), -1, 1))
-    opt = tf.keras.optimizers.Adam(learning_rate=0.0302)
+    opt = tf.keras.optimizers.SGD(learning_rate=0.0302)
     for _ in range(97):
         with tf.GradientTape() as tape:
             loss = f(x)
         grads = tape.gradient(loss, [x])
         opt.apply_gradients(zip(grads, [x]))
-    return x
-
+    return tf.Variable(initial_value=x.numpy(), dtype=tf.float32)
